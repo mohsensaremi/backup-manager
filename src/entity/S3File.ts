284b535d6cd8a237a24d64a582a4ae797523290a
mohsen saremi <mohsen.sareminia@gmail.com>
2022-06-30 22:45:43 +0430

    add storage name in S3 file

--- src/entity/S3File.ts
+++ src/entity/S3File.ts
@@ -2,11 +2,11 @@ import { _Object } from '@aws-sdk/client-s3';
 import { StorageFile } from './StorageFile';
 
 export class S3File extends StorageFile {
-  constructor(public readonly obj: _Object) {
+  constructor(readonly storageName: string, readonly obj: _Object) {
     if (!obj.Key) {
       throw new Error('s3 file Key is required');
     }
-    super(obj.Key);
+    super(storageName, obj.Key);
   }
 
   async size() {
